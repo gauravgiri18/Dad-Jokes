0a39dce286b451c327eabb05a76ac1daaf469333
Gaurav <gauravv9958@gmail.com>
2024-03-13 01:15:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('#btn');
+const clearButton = document.querySelector('#clrbtn');
+
+const addNewJokes = async () => {
+    const jokeText = await getDadJoke();
+    const newLi = document.createElement('li');
+    newLi.append(jokeText);
+    jokes.append(newLi);
+}
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: "application/json" } }
+        const res = await axios.get('https://icanhazdadjoke.com', config);
+        return res.data.joke;
+    } catch (e) {
+        return "SORRY!! No Jokes Available :(";
+    }
+
+}
+
+const clearJoke = () => {
+    jokes.innerHTML = "";
+}
+
+button.addEventListener('click', addNewJokes);
+clearButton.addEventListener('click', clearJoke);
+
+
+// const jokes = document.querySelector("#jokes");
+// const button = document.querySelector('#btn');
+// const clearButton = document.querySelector('#clrbtn');
+
+
+// const addNewJoke = async () => {
+//     const jokeText = await getDadJokes();
+//     const newLI = document.createElement('li');
+//     newLI.append(jokeText);
+//     jokes.append(newLI);
+
+// }
+// const getDadJokes = async () => {
+//     try {
+//         const config = { headers: { Accept: 'application/json' } }
+//         const res = await axios.get('https://icanhazdadjoke.com', config);
+//         return res.data.joke;
+//     } catch (e) {
+//         return "Sorry No Jokes Available :("
+//     }
+// }
+
+// const clearJokes = () => {
+//     jokes.innerHTML = "";
+// }
+
+// button.addEventListener('click', addNewJokes);
+// clearButton.addEventListener('click', clearJokes);
+
